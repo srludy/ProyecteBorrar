10e7a840613ae58d980ae86763827563630c4764
2dam <2dam@WINMAC21.alumnado.flo>
2017-09-26 17:24:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a2dam/proyecteborrar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a2dam.proyecteborrar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
